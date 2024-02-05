e9a77cdd7d966ab0a0121df364b1f3ab68d3dd32
RamazanTemirkayevich <r.bakshishov@gmail.com>
2024-02-05 17:30:43 +0300

    header with menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,35 @@
 import sayHello from './lib/sayHello';
 
 sayHello();
+
+$(document).on('click', '.parent-sub', function () {
+    const subMenu = document.querySelector('.nav-wrap__menu-sub__list');
+    const arrow = document.querySelector('.arrow-icon')
+
+    subMenu.classList.toggle('active')
+    arrow.classList.toggle('active')
+})
+
+function burgerMenu() {
+    const burger = document.querySelector(".header-wrap__menu");
+    const nav = document.querySelector(".nav");
+    const {body} = document;
+    
+    if (burger && nav) {
+        burger.addEventListener("click", () => {
+            burger.classList.toggle("active");
+            nav.classList.toggle("active");
+            body.classList.toggle("lock");
+        });
+    }
+};
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 50) {
+        $('.header').addClass('header--scroll');
+    } else {
+        $('.header').removeClass('header--scroll');
+    }
+});
+
+burgerMenu();
